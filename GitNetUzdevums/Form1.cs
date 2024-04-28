5145ca7a40c22202c7fce01b2397a01090c73157
Alina Trofimovica <linakees2003@gmail.com>
2024-04-28 20:48:11 +0300

    Added graphic function

--- GitNetUzdevums/Form1.cs
+++ GitNetUzdevums/Form1.cs
@@ -77,5 +77,27 @@ namespace WindowsFormsApplication_15
                 x += step;
             }
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int radius = 100; // Set the radius of the circle
+            int xc = pictureBox1.Width / 2;   // center coordinates
+            int yc = pictureBox1.Height / 2;
+            double step = 0.01;    // step in radians for drawing the circle
+            Graphics G = pictureBox1.CreateGraphics();
+            G.Clear(System.Drawing.Color.White);
+            Pen myPen = new Pen(Color.Black);
+
+            // Drawing circle using parametric form
+            double theta = 0; // Start angle in radians
+            while (theta < 2 * Math.PI) // Full circle
+            {
+                int xe = (int)(xc + radius * Math.Cos(theta)); // x = xc + r*cos(theta)
+                int ye = (int)(yc + radius * Math.Sin(theta)); // y = yc + r*sin(theta)
+                G.DrawEllipse(myPen, xe, ye, 1, 1); // Draw small ellipse (dot) at (xe, ye)
+
+                theta += step; // Increase theta by step
+            }
+        }
     }
 }
